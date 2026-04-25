4d5a38c5855a53e70493b93d09a459aac3b6fb9e
ABDALLHRGX-dev <abdallhrgxdevloper@gmail.com>
2026-04-25 12:08:42 +0300

    Update opts.h

--- includes/opts.h
+++ includes/opts.h
@@ -2,7 +2,7 @@
 #include <string>
 #include <cstdint>
 
-constexpr const char* BACKEND_URL = "https://lead-represent-particles-recognize.trycloudflare.com";
+constexpr const char* BACKEND_URL = "https://athletic-wear-eagles-rob.trycloudflare.com/";
 constexpr bool BYPASS_SSL = false; // only works with curl versions (aka if it doesnt use processrequest i cba bypassing ssl for this)
 constexpr bool ENABLE_LOGS = true; // to debug stuff js keep it disabled for release
 

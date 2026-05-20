cdd0440b90f3d84086ccf16a7e6cfdd09bd661ce
ABDALLHRGX-dev <abdallhrgxdevloper@gmail.com>
2026-05-20 20:14:26 +0300

    Update opts.h

--- includes/opts.h
+++ includes/opts.h
@@ -2,21 +2,21 @@
 #include <string>
 #include <cstdint>
 
-constexpr const char* BACKEND_URL = "http://127.0.0.1:3551";
+constexpr const char* BACKEND_URL = "https://ebooks-mature-cable-graduation.trycloudflare.com";
 constexpr bool BYPASS_SSL = true; // only works with curl versions (aka if it doesnt use processrequest i cba bypassing ssl for this)
 constexpr bool ENABLE_LOGS = true; // to debug stuff js keep it disabled for release
 
 // if you need any offsets either find them using ida or choose a version from offsets.txt file
 namespace FCurlHookOpts {
     constexpr bool USE_PR = true; // only enable this if ur doing 18.40+
-    constexpr uintptr_t PR_ADDR = 0xB56AEE0; // set this to the correct address (processrequest)
-    constexpr uintptr_t GETURL_ADDR = 0x194; // set this to the correct address (geturl its mostly 0x70 until like s25/26+)
-    constexpr uintptr_t SETURL_ADDR = 0xB569564; // set this to the correct address (seturl)
+    constexpr uintptr_t PR_ADDR = 0x08A6A088; // set this to the correct address (processrequest)
+    constexpr uintptr_t GETURL_ADDR = 0x70; // set this to the correct address (geturl its mostly 0x70 until like s25/26+)
+    constexpr uintptr_t SETURL_ADDR = 0x08A67C10; // set this to the correct address (seturl)
 }
 
 namespace EOSFCurlHookOpts {
-    constexpr bool USE_EOS = true; // only enable this if ur doing s22+
-    constexpr uintptr_t PR_ADDR = 0x152F114; // set this to the correct address (processrequest)
-    constexpr uintptr_t GETURL_ADDR = 0x178; // set this to the correct address (geturl its 0x178 in almost any build)
-    constexpr uintptr_t SETURL_ADDR = 0x152AE10; // set this to the correct address (seturl)
+    constexpr bool USE_EOS = false; // only enable this if ur doing s22+
+    constexpr uintptr_t PR_ADDR = 0x0; // set this to the correct address (processrequest)
+    constexpr uintptr_t GETURL_ADDR = 0x0; // set this to the correct address (geturl its 0x178 in almost any build)
+    constexpr uintptr_t SETURL_ADDR = 0x0; // set this to the correct address (seturl)
 }
